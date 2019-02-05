0c02a2b098cc9a19ca9378b84c1d2740a4d11198
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-02-05 19:10:59 -0200

    Transformação do array para computar os totalizadores

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,10 +1,14 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 document
   .querySelector('#myButton')
-  .onclick = () => {
+  .onclick = () =>
     fetch('http://localhost:3000/notas')
       .then(handleStatus)
-      .then(notas => console.log(notas))
+      .then(notas => notas.reduce((array, nota) => array.concat(nota.itens), []))
+      .then(log)
+      .then(itens => itens.filter(item => item.codigo == '2143'))
+      .then(log)
+      .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+      .then(console.log)
       .catch(console.log);
-  }
