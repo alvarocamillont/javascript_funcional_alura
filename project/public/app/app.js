fef9ce6d61c27750ba93332af35479afff8d3080
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-02-07 18:31:35 -0200

    realização da composição da função

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,16 +1,19 @@
 
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+)
 
-const action = debounceTime(500, takeUntil(3, () =>
+const action = operations(() =>
   service
     .sumItems('2143')
     .then(console.log)
     .catch(console.log)
-));
-
+);
 document
   .querySelector('#myButton')
   .onclick = action;
