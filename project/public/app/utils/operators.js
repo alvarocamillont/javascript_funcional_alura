0629b01100cb0e33de9104c326991196b725220c
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-02-07 18:10:25 -0200

    Criação da função takeUNTIL

--- project/public/app/utils/operators.js
+++ project/public/app/utils/operators.js
@@ -9,3 +9,7 @@ export const compose = (...fns) => value =>
 export const pipe = (...fns) => value =>
   fns.reduce((previousValue, fn) =>
     fn(previousValue), value);
+
+export const takeUntil = (times, fn) => {
+  return () => times-- > 0 && fn();
+}
