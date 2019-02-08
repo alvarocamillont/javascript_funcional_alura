10cb30a1d891fd0971c556e1a29dbc7741c099b4
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-02-08 13:36:05 -0200

    Criado mecanismo de timetout

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,19 +1,21 @@
 
+import { timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
 const operations = pipe(
   partialize(takeUntil, 3),
-  partialize(debounceTime, 500)
-)
+  partialize(debounceTime, 500),
+);
 
+// usando timeoutPromise
 const action = operations(() =>
-  service
-    .sumItems('2143')
+  timeoutPromise(200, service.sumItems('2143'))
     .then(console.log)
     .catch(console.log)
 );
+
 document
   .querySelector('#myButton')
   .onclick = action;
